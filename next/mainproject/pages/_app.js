cec052edb227c0c8e73b95cd98748790b6177e34
vahidkk <kohandel.vahid@gmail.com>
2021-10-12 11:12:34 +0330

    Bootstrap added to project

--- next/mainproject/pages/_app.js
+++ next/mainproject/pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/dist/shared/lib/head";
+
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
