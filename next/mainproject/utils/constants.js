13bb4baaab90a051b556d34602ae1b547b5ebc54
vahidkk <kohandel.vahid@gmail.com>
2022-01-18 17:27:28 +0330

    final deploy v2

--- next/mainproject/utils/constants.js
+++ next/mainproject/utils/constants.js
@@ -1,6 +1,6 @@
 // export const localhost = "https://vahid-online-store.herokuapp.com/";
 
-// export const localhost = "http://127.0.0.1:8000/";
-export const localhost = "https://ancient-temple-08846.herokuapp.com/";
+export const localhost = "http://127.0.0.1:8000/";
+// export const localhost = "https://ancient-temple-08846.herokuapp.com/";
 export const apiURL = "api";
 export const endpoint = `${localhost}${apiURL}`;
