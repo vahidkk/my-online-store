f9d0c2ecf50d7c19ffba21de0cb2056d87bac5b1
vahidkk <kohandel.vahid@gmail.com>
2022-01-18 11:13:53 +0330

    final deploy v1.6

--- next/mainproject/next.config.js
+++ next/mainproject/next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["127.0.0.1"],
+    domains: [
+      "127.0.0.1",
+      "my-online-store.vercel.app",
+      "ancient-temple-08846.herokuapp.com",
+    ],
   },
 };
